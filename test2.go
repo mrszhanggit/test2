bfc9dcdf67d0589ccb3b6360d65a5613aa82782c
zhangwang <794514808@qq.com>
2022-10-30 21:31:00 +0800

    a

--- test2.go
+++ test2.go
@@ -3,15 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("3333333")
-	fmt.Println("444444")
-	fmt.Println("2222222")
-	fmt.Println("9999999")
-	fmt.Println("101010")
-	fmt.Println("121212")
-	fmt.Println("5555555")
-	fmt.Println("6666666")
-	fmt.Println("77777777")
-	fmt.Println("88888888")
-	fmt.Println("99999999")
+
+	fmt.Println("Hello World")
+
 }
